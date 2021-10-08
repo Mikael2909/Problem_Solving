1f679b6694e5700371486e91d4eba30396087dcc
Mikael Petrus Saragi <mikaelsaragi29@gmail.com>
2021-10-08 23:45:40 +0700

    add problem 2

---
+++ Assets/Script/Problem 2/BallControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    public float xForce;
+    public float yForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void PushBall()
+    {
+        float arahRandom = Random.Range(0, 2);
+        if (arahRandom < 1.0f)
+        {
+            rigidbody2D.AddForce(new Vector2(-xForce, yForce));
+        }
+        else
+        {
+            rigidbody2D.AddForce(new Vector2(xForce, yForce));
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        PushBall();
+    }
+}
