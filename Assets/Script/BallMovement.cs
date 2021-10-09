68fab21ad85d05398d594f2ed51676300fb47bc2
Mikael Petrus Saragi <mikaelsaragi29@gmail.com>
2021-10-09 14:37:14 +0700

    add scene problem 4

---
+++ Assets/Script/BallMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D playerRigidbody;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");//tombol a,d
+        playerRigidbody.velocity = new Vector2(speed * horizontal, playerRigidbody.velocity.y);
+        float vertical = Input.GetAxisRaw("Vertical");//tombol w,s
+        playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, speed*vertical);
+    }
+}
