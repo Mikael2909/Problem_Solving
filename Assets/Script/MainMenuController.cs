5dbb4d1709e595e0e05b29aa96afc73997d1ee13
Mikael Petrus Saragi <mikaelsaragi29@gmail.com>
2021-10-12 00:26:18 +0700

    final build

---
+++ Assets/Script/MainMenuController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    
+    [SerializeField] private Button bt1;
+    [SerializeField] private Button bt2;
+    [SerializeField] private Button bt3;
+    [SerializeField] private Button bt4;
+    [SerializeField] private Button bt5;
+    [SerializeField] private Button bt6;
+    [SerializeField] private Button bt7;
+    [SerializeField] private Button bt8;
+    [SerializeField] private Button bt9;
+
+    private void Start()
+    {
+        bt1.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        bt2.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(2);
+        });
+
+        bt3.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(3);
+        });
+
+        bt4.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(4);
+        });
+
+        bt5.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(5);
+        });
+
+        bt6.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(6);
+        });
+
+        bt7.onClick.AddListener(() =>
+       {
+            SceneManager.LoadScene(7);
+        });
+
+        bt8.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(8);
+        });
+
+        bt9.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(9);
+        });
+    }
+   
+}
---
+++ Assets/Script/Problem8/ObstacleSpawnerProblem8.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawnerProblem8 : MonoBehaviour
+{
+    public GameObject kotak;
+    public float xMin, xMax, yMin, yMax;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Spawn();
+        StartCoroutine(SpawnKotak());
+    }
+    void Spawn()    
+    {
+        int penghitungObjek = Random.Range(5, 10);
+        for (int i = 0; i < penghitungObjek; i++)
+        {
+            float randomX = Random.Range(xMin, xMax);
+            float randomY = Random.Range(yMin, yMax);
+            Instantiate(kotak, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+       
+    }
+    IEnumerator SpawnKotak()
+    {
+        if (transform.childCount < 10)
+        {
+            float randomX = Random.Range(xMin, xMax);
+            float randomY = Random.Range(yMin, yMax);
+
+            Instantiate(kotak, new Vector2(randomX, randomY), Quaternion.identity); 
+            
+        }
+        yield return new WaitForSeconds(3);
+        StartCoroutine(SpawnKotak()); 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
